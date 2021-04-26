da4605eb69db173706e62918eb43ef83c20e0979
Joel Alenchery <joel.alenchery@nexsales.com>
2021-04-26 19:50:38 +0530

    pin timezones to IST

--- scraper/bin/cron.js
+++ scraper/bin/cron.js
@@ -2,22 +2,28 @@ const CronJob = require("cron").CronJob;
 const pm2 = require("pm2");
 
 console.log("Before job instantiation");
-const job = new CronJob("0 */30 6-23 * * *", function () {
-  const d = new Date();
-  console.log("Running script:", d);
-  pm2.start(
-    {
-      script: "npm -- run full-cycle",
-      autorestart: false,
-    },
-    (err, apps) => {
-      pm2.disconnect();
-      if (err) {
-        throw err;
+const job = new CronJob(
+  "0 */30 6-23 * * *",
+  function () {
+    const d = new Date();
+    console.log("Running script:", d);
+    pm2.start(
+      {
+        script: "npm -- run full-cycle",
+        autorestart: false,
+      },
+      (err, apps) => {
+        pm2.disconnect();
+        if (err) {
+          throw err;
+        }
       }
-    }
-  );
-});
+    );
+  },
+  null,
+  false,
+  "Asia/Kolkata"
+);
 console.log("After job instantiation");
 job.start();
 console.log("is job running? ", job.running);
--- scraper/bin/zip-data.sh
+++ scraper/bin/zip-data.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-now=$(date +"%Y-%m-%d_%H-%M")
+now=$(env TZ=Asia/Calcutta date +"%Y-%m-%d_%H-%M")
 zip -r --exclude=*.DS_Store* ./data-zip/data_$now.zip ./data-raw
