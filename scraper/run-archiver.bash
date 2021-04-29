20b73023df215e81870a1422c7f48dc9fac9998e
YuviPanda <yuvipanda@gmail.com>
2021-04-30 01:35:05 +0530

    Push to data repo with git
    ssh deploy key has been setup manually

--- scraper/run-archiver.bash
+++ scraper/run-archiver.bash
@@ -3,10 +3,14 @@
 # The archiver itself takes about 5min, so we just wait 5min between runs
 # Intentionally not setting -e, since we just want it to continue even if the process fails
 
-mkdir -p archive
-cd archive
+cd VaccinationSlotsArchive/archive
 while true; do
-    node ../archiver.js
+    node ../../archiver.js
+    # Update our checkout before we commit - avoids push rejections
+    git pull origin main
+    git add .
+    git -c 'user.email=Archiver@archiver.archive' -c 'user.name=archiver' commit -m 'Adding data' 
+    git push origin main
     echo 'sleeping...'
     sleep 5m
 done
