233461742a2f5099b7647645b3ef8af33f822324
YuviPanda <yuvipanda@gmail.com>
2021-04-30 01:47:47 +0530

    Fetch list of states & districts from same API
    Makes sure we don't end up with weird mismatches of
    ids because we're using state & district data from two
    different APIs

--- scraper/cvc.js
+++ scraper/cvc.js
@@ -21,46 +21,6 @@ const SHEET_ID = "1NR36K5nBy4rI69qU6dc5MGt2QwwTXZugDnjlWnei3X8"
 const LOCATION_SHEET_ID = "1iC7Ai5mATnPTHuP9eSTEWJ8GLZKLWCh5fdyVRcqblsU"
 const EIGHTEEN_SHEET = '1utlRNu58guwjK8sgN9woQClTnCS0O2J8OKD_eey2FTE'
 
-const STATE_IDS = [
-  { name: "Andaman and Nicobar Islands", id: "1" },
-  { name: "Andhra Pradesh", id: "2" },
-  { name: "Arunachal Pradesh", id: "3" },
-  { name: "Assam", id: "4" },
-  { name: "Bihar", id: "5" },
-  { name: "Chandigarh", id: "6" },
-  { name: "Chhattisgarh", id: "7" },
-  { name: "Dadra and Nagar Haveli", id: "8" },
-  { name: "Daman and Diu", id: "37" },
-  { name: "Delhi", id: "9" },
-  { name: "Goa", id: "10" },
-  { name: "Gujarat", id: "11" },
-  { name: "Haryana", id: "12" },
-  { name: "Himachal Pradesh", id: "13" },
-  { name: "Jammu and Kashmir", id: "14" },
-  { name: "Jharkhand", id: "15" },
-  { name: "Karnataka", id: "16" },
-  { name: "Kerala", id: "17" },
-  { name: "Ladakh", id: "18" },
-  { name: "Lakshadweep", id: "19" },
-  { name: "Madhya Pradesh", id: "20" },
-  { name: "Maharashtra", id: "21" },
-  { name: "Manipur", id: "22" },
-  { name: "Meghalaya", id: "23" },
-  { name: "Mizoram", id: "24" },
-  { name: "Nagaland", id: "25" },
-  { name: "Odisha", id: "26" },
-  { name: "Puducherry", id: "27" },
-  { name: "Punjab", id: "28" },
-  { name: "Rajasthan", id: "29" },
-  { name: "Sikkim", id: "30" },
-  { name: "Tamil Nadu", id: "31" },
-  { name: "Telangana", id: "32" },
-  { name: "Tripura", id: "33" },
-  { name: "Uttar Pradesh", id: "34" },
-  { name: "Uttarakhand", id: "35" },
-  { name: "West Bengal", id: "36" },
-];
-
 
 class State {
   constructor(id, name, districts) {
@@ -370,8 +330,14 @@ async function eighteen(states, days) {
 }
 
 async function getAllStates() {
-  const districts = await getRawDistricts()
-  return STATE_IDS.map(({ id, name }) => new State(id, name, districts.filter(d => d.state_id == id)))
+  const url = 'https://api.cowin.gov.in/api/v2/admin/location/states';
+  const states = (await axios.get(url)).data.states
+
+  return await pMap(states, async ({ state_id, state_name }) => {
+    // FIXME: Ensure id is an int
+    const distUrl = `https://api.cowin.gov.in/api/v2/admin/location/districts/${state_id}`;
+    return new State(state_id, state_name, (await axios.get(distUrl)).data.districts)
+  })
 }
 
 async function main() {
